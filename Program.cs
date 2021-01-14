b39493a8d26aee5620a2a97437cfabad173eba22
miran rashid <miranrashid@outlook.com>
2021-01-14 20:36:36 +0300

    code with comment

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace date_of_birth_calculator
+{
+    class Program
+    {
+        public static void birth_calculator(int age)
+        {
+            
+            int year = 2021-age;
+            
+            Console.WriteLine("your year of birth is " + year);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter you age");
+
+            birth_calculator(Convert.ToInt32 (Console.ReadLine()));
+
+            //change for a commit on git ,just to know how it work
+
+        }
+    }
+}
